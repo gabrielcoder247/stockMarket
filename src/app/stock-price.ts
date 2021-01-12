9912752602b39e80e96569ab05e5da4f607814ff
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-01-12 14:10:34 +0100

    initial commit

---
+++ src/app/stock-price.ts
@@ -0,0 +1,8 @@
+// export class StockPrice {
+// }
+
+export class StockPrice {
+    constructor(public login: string, public name:string,  public avatar_url:string,  public html_url: string,
+        public created_at:Date){
+      }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+// export const environment = {
+//   production: true
+// };
+
+
 export const environment = {
-  production: true
+  production: true,
+  // apiUrl:"http://api.giphy.com/v1/gifs/random",
+  apiUrl:"https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo",
+  token :"I3K8HTMAMTSEE3S8"
 };
